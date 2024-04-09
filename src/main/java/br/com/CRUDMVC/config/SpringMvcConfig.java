76a639ab67b54758c2287f097e3b814f74dda66c
AdrianoBarbosaDev <adrianobarbosa1999@gmail.com>
2024-04-08 22:11:31 -0300

    first commit

---
+++ src/main/java/br/com/CRUDMVC/config/SpringMvcConfig.java
@@ -0,0 +1,38 @@
+package br.com.CRUDMVC.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import br.com.CRUDMVC.conversor.TipoSexoConverter;
+
+@Configuration
+public class SpringMvcConfig extends WebMvcConfigurerAdapter{
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addConverter(new TipoSexoConverter());
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("messages");
+		return source;
+	}
+	
+}
